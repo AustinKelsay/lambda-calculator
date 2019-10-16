e34f6a92b7461115cf9a47045a218334a62b431e
AustinKelsay <austinkelsay@yahoo.com>
2019-10-16 14:58:14 -0500

    Stuck on step 3

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {numbers} from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -7,13 +8,22 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
+const mapper = numbers.map((item) => {
+  return <button>{item}</button>
+})
+
+console.log(mapper);
+
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {mapper}
+      
     </div>
   );
 };
